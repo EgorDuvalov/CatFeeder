0f3eb20748ca18fe1e296d48b3b6dbc3357dd56c
Vladislav Atroshkin <v.atroshkin@itransition.com>
2021-12-16 23:47:16 +0300

    bug fixed

--- src/main/java/com/bsu/catfeeder/mapper/LogMapper.java
+++ src/main/java/com/bsu/catfeeder/mapper/LogMapper.java
@@ -3,10 +3,14 @@ package com.bsu.catfeeder.mapper;
 import com.bsu.catfeeder.dto.LogDto;
 import com.bsu.catfeeder.entity.Log;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface LogMapper {
+
+    @Mapping(source = "creationTime", target = "creationTime")
+    @Mapping(source = "message", target = "message")
     List<LogDto> mapToLogDtoList(List<Log> logList);
 }
